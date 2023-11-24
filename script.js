b30268b0718471055591ecc8bbd9b2df2f3fd179
Your Name <new.anaspakattippara@gmail.com>
2023-11-24 15:40:18 +0530

    abbas

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const person = {
-    profile_pic: "./image/man19152370.jpg",
-    profile_logo: "./image/navigo.jpg",
+    profile_pic: "./image/ABBAS.jpg",
+    profile_logo: "./image/PATTA.jpeg",
 
-    Name: "AMEERSHA MOHAMMED",
+    Name: "Abbas Bankoda",
     designation: "General Manager",
-    des_link_text: "@Fort_Oasis_Trading ",
+    des_link_text: "@ Al Mufrid Trading LLC ",
     des_link: "",
-    contact: "./contact/Ameersha Mohammed.vcf",
+    contact: "./contact/Abbas Bankoda.vcf",
 };
 
 const contact ={
-    contact_no: "+971504584154",
-    whatsapp: "+971504584154",
-    location: "",
-    email: "achuthodika@gmail.com",
+    contact_no: "+971526415341",
+    whatsapp: "+971526415341",
+    location: "https://www.google.ae/maps/place/Al+Mufrid+Building+Materials+trading+LLC/@25.2694783,55.3034339,17z/data=!3m1!4b1!4m6!3m5!1s0x3e5f43b6fbd050ab:0xbae5ffb12680c866!8m2!3d25.2694735!4d55.3060088!16s%2Fg%2F11j5qgtyy4?entry=ttu",
+    email: "almufrid13@gmail.com",
 };
 
 const profilelogo=[
   logo={
-    visibil: false,
+    visibil: true,
   }
 ]
 
@@ -30,8 +30,8 @@ const links =[
     link:"https://www.facebook.com/navigotravels?mibextid=LQQJ4d"
   },
     instagram={
-    visibil: false,
-    link:"https://instagram.com/navigo_trips?igshid=OGQ5ZDc2ODk2ZA=="
+    visibil: true,
+    link:"https://instagram.com/patta_almufrid_dubai"
   },
     skype={
     visibil: false,
@@ -55,7 +55,7 @@ const links =[
   },
     mail={
     visibil: true,
-    link:"achuthodika@hotmail.com"
+    link:"almufrid13@gmail.com"
   },
     google={
     visibil: false,
