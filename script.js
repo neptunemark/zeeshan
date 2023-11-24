00d7448f031a6265e6fe70e5d9c7593c1bb7961e
Your Name <new.anaspakattippara@gmail.com>
2023-11-24 16:02:37 +0530

    hanif

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const person = {
-    profile_pic: "./image/ABBAS.jpg",
-    profile_logo: "./image/PATTA.jpeg",
+    profile_pic: "./image/hanif.jpeg",
+    profile_logo: "./image/noufal.jpeg",
 
-    Name: "Abbas Bankoda",
-    designation: "General Manager",
-    des_link_text: "@ Al Mufrid Trading LLC ",
+    Name: "Muhammad Hanif",
+    designation: "Owner",
+    des_link_text: "@ Naufal typing  ",
     des_link: "",
-    contact: "./contact/Abbas Bankoda.vcf",
+    contact: "./contact/Muhammad Hanif.vcf",
 };
 
 const contact ={
-    contact_no: "+971526415341",
-    whatsapp: "+971526415341",
-    location: "https://www.google.ae/maps/place/Al+Mufrid+Building+Materials+trading+LLC/@25.2694783,55.3034339,17z/data=!3m1!4b1!4m6!3m5!1s0x3e5f43b6fbd050ab:0xbae5ffb12680c866!8m2!3d25.2694735!4d55.3060088!16s%2Fg%2F11j5qgtyy4?entry=ttu",
-    email: "almufrid13@gmail.com",
+    contact_no: "+97145487687",
+    whatsapp: "+971562108012",
+    location: "",
+    email: "Naufaltyping@gmail.com",
 };
 
 const profilelogo=[
@@ -30,7 +30,7 @@ const links =[
     link:"https://www.facebook.com/navigotravels?mibextid=LQQJ4d"
   },
     instagram={
-    visibil: true,
+    visibil: false,
     link:"https://instagram.com/patta_almufrid_dubai"
   },
     skype={
@@ -55,7 +55,7 @@ const links =[
   },
     mail={
     visibil: true,
-    link:"almufrid13@gmail.com"
+    link:"Naufaltyping@gmail.com"
   },
     google={
     visibil: false,
